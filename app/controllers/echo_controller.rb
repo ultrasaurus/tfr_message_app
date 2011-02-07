4b21a116d1ac00858cbd5eb1d1a8eaa63d615c99
Sarah Allen <sarah@ultrasaurus.com>
2011-02-06 23:42:39 -0800

    added rspec and spec for EchoController

---
+++ app/controllers/echo_controller.rb
@@ -0,0 +1,2 @@
+class EchoController < ApplicationController
+end
---
+++ spec/controllers/echo_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe EchoController do
+  describe "GET index" do
+    it "renders" do
+      get :index        #<label id="code.get.index"/>
+      response.should render_template(:echo) #<label id="code.should.render"/>
+    end
+     
+    it "renders with text param" do
+      sample_text = "something"      #<label id="code.local.sample_text"/>
+      get :index, {:text => sample_text}    #<label id="code.get.text.param"/>
+      assigns[:message].should == sample_text   #<label id="code.assigs.message"/>
+      response.should render_template(:echo)
+    end
+  end
+
+end

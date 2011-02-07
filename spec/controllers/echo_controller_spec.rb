6c0b92cccf03491b7f4a0065651810e9b81428f5
Sarah Allen <sarah@ultrasaurus.com>
2011-02-07 00:03:47 -0800

    fixed typo -- should render :index

--- spec/controllers/echo_controller_spec.rb
+++ spec/controllers/echo_controller_spec.rb
@@ -4,14 +4,14 @@ describe EchoController do
   describe "GET index" do
     it "renders" do
       get :index        #<label id="code.get.index"/>
-      response.should render_template(:echo) #<label id="code.should.render"/>
+      response.should render_template(:index) #<label id="code.should.render"/>
     end
      
     it "renders with text param" do
       sample_text = "something"      #<label id="code.local.sample_text"/>
       get :index, {:text => sample_text}    #<label id="code.get.text.param"/>
       assigns[:message].should == sample_text   #<label id="code.assigs.message"/>
-      response.should render_template(:echo)
+      response.should render_template(:index)
     end
   end
 
